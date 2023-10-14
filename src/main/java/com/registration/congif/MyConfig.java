2dab5ef2fb5aaad8f5a88a98ff296d2504308476
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-10-14 16:40:55 +0530

    Myconfig class create and modelmapper bean defination written

---
+++ src/main/java/com/registration/congif/MyConfig.java
@@ -0,0 +1,21 @@
+package com.registration.congif;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+
+
+
+           @Bean
+           public ModelMapper modelMapper(){
+
+               return new ModelMapper();
+           }
+
+
+
+
+}
