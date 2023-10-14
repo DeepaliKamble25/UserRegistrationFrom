962692ce159dd8e6fe64e7dc6ba8a57f2b7f10d7
DeepaliKamble25 <deepalikamble25@gmail.com>
2023-10-14 16:39:31 +0530

    UserService method written

---
+++ src/main/java/com/registration/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,34 @@
+package com.registration.service.impl;
+
+import com.registration.dao.CompanyDto;
+import com.registration.service.CompanyService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+  private
+
+
+    @Override
+    public CompanyDto createCompanydata(CompanyDto companyDto) {
+        return null;
+    }
+
+    @Override
+    public CompanyDto updateRecordCompany(Long userId, Long companyId) {
+        return null;
+    }
+
+    @Override
+    public void deleteCompanydetailByUserDto(Long userId, Long companyId) {
+
+    }
+
+    @Override
+    public List<CompanyDto> getall(Long userId) {
+        return null;
+    }
+}
